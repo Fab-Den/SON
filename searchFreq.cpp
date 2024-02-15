5e9a9a9fdf235bf0260793c0eaa5ff8238ebeb53
Lpoulhe <154341987+Lpoulhe@users.noreply.github.com>
2024-02-15 10:25:19 +0100

    search Freq

---
+++ searchFreq.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+
+float searchFreq(float note, float freqs[60]) {
+    float closest = freqs[0];
+    float min_diff = abs(note - freq[0]);
+
+    for (int i = 0; i<60, i++) {
+        float diff = abs(freqs[i]-note);
+        if (diff<min_diff) {
+            min_diff = diff;
+            closest = freqs[i];
+        }
+        if (min_diff == 0) {
+            break;
+        }
+    }
+    return closest;
+}
